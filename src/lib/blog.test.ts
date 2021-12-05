c213ba630ec21cb96276fee889eed9119411bcfa
Shuntaro Nishizawa <me@shun.technology>
2021-12-05 15:50:30 +0900

    test: Add `getArticle` test

--- src/lib/blog.test.ts
+++ src/lib/blog.test.ts
@@ -3,9 +3,20 @@ import { getArticlePath } from "./blog";
 describe("getArticlePath", () => {
   it("特定のディレクトリ下のMarkdownファイルのパスを全件取得する", async () => {
     expect(await getArticlePath("tests/articles")).toStrictEqual([
-      "20211205/test1.md",
-      "20211205/test2.md",
-      "20211206/test3.md",
+      "2021-12-05/test1.md",
+      "2021-12-05/test2.md",
+      "2021-12-06/test3.md",
     ]);
+  });
+});
+
+describe("getArticle", () => {
+  it("特定のMarkdownファイルをパースして、記事オブジェクトを取得する", async () => {
+    const article = await getArticle("tests/articles", "2021-12-05/test1.md");
+
+    expect(article.slug).toBe("2021-12-05-test1");
+    expect(article.title).toBe("Test Article 1");
+    expect(article.postedAt).toBe("2021-12-05T00:00:00.000Z");
+    expect(article.tags).toStrictEqual(["foo", "bar"]);
   });
 });
