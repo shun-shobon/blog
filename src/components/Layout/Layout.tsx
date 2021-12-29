93dbd5b0f74a650a72913bf93c516d4caee60057
Shuntaro Nishizawa <me@shun.technology>
2021-12-30 01:49:25 +0900

    feat: Add header link

--- src/components/Layout/Layout.tsx
+++ src/components/Layout/Layout.tsx
@@ -1,4 +1,5 @@
 import { type ReactNode } from "react";
+import Link from "next/link";
 
 export type Props = {
   children: ReactNode;
@@ -7,9 +8,13 @@ export type Props = {
 export const Layout = ({ children }: Props): JSX.Element => {
   return (
     <>
-      <header className="py-8 text-center">
-        <h1 className="text-2xl">高専生はエンジニアの夢を見るか？</h1>
-        <h2 className="text-gray-500">しがない高専生の雑多ブログ</h2>
+      <header className="py-8 grid place-items-center">
+        <Link href="/">
+          <a className="text-center hover:opacity-50">
+            <h1 className="text-2xl">高専生はエンジニアの夢を見るか？</h1>
+            <h2 className="text-gray-500">しがない高専生の雑多ブログ</h2>
+          </a>
+        </Link>
       </header>
       {children}
       <footer className="py-2 border-t border-gray-300 text-center text-gray-500">
