076239abb95d0bfdff450dfd224c7b0a206b84c8
Shuntaro Nishizawa <me@shun.technology>
2021-12-29 23:01:27 +0900

    feat: Add article page

---
+++ src/pages/articles/[slug].tsx
@@ -0,0 +1,73 @@
+import { type NextPage, type GetStaticProps, type GetStaticPaths } from "next";
+import { format } from "date-fns";
+import * as E from "fp-ts/Either";
+
+import { type Article, fetchArticles } from "../../lib/blog";
+import { getConfig } from "../../config";
+
+import { Layout } from "../../components/Layout";
+import { Markdown } from "../../components/Markdown";
+
+type Props = {
+  article: Article;
+};
+
+const Article: NextPage<Props> = ({ article }) => {
+  return (
+    <Layout>
+      <main className="container mx-auto p-4 grid gap-8">
+        <div>
+          <h1 className="text-2xl text-cyan-500">{article.title}</h1>
+          <p className="text-gray-500 flex flex-wrap gap-2">
+            {article.tags.map((tag) => (
+              <span key={tag}>#{tag}</span>
+            ))}
+          </p>
+          <p className="text-gray-500 text-right">
+            {format(new Date(article.postedAt), "yyyy年MM月dd日")}
+          </p>
+        </div>
+        <Markdown contents={article.contents} />
+      </main>
+    </Layout>
+  );
+};
+
+export const getStaticProps: GetStaticProps<Props> = async ({ params }) => {
+  const slug = params?.["slug"];
+  if (typeof slug !== "string") return { notFound: true };
+
+  const { articlePath } = getConfig();
+
+  const eitherArticles = await fetchArticles(articlePath);
+  if (E.isLeft(eitherArticles)) throw new Error("Failed to fetch articles");
+
+  const articles = eitherArticles.right;
+
+  const article = articles.find((article) => article.slug === slug);
+  if (typeof article === "undefined") return { notFound: true };
+
+  return {
+    props: {
+      article,
+    },
+  };
+};
+
+export const getStaticPaths: GetStaticPaths = async () => {
+  const { articlePath } = getConfig();
+
+  const eitherArticles = await fetchArticles(articlePath);
+  if (E.isLeft(eitherArticles)) throw new Error("Failed to fetch articles");
+
+  const articles = eitherArticles.right;
+
+  const slugs = articles.map(({ slug }) => slug);
+
+  return {
+    paths: slugs.map((slug) => ({ params: { slug } })),
+    fallback: "blocking",
+  };
+};
+
+export default Article;
