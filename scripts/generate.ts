8754f5d6fd2d0f04405d3e4b79266ae7a8373382
Shuntaro Nishizawa <me@shun.technology>
2023-02-28 11:43:13 +0900

    build: Add build scripts

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -1,6 +1,8 @@
 import { ARTICLE_DATA_DIR, ARTICLE_IMAGE_DIR, ARTICLES_DIR } from "@/config";
 import { exportArticles } from "@/lib/markdown";
 
-void exportArticles("test-articles", ARTICLE_IMAGE_DIR, ARTICLE_DATA_DIR).catch(
-  console.error,
-);
+async function main() {
+  await exportArticles(ARTICLES_DIR, ARTICLE_IMAGE_DIR, ARTICLE_DATA_DIR);
+}
+
+void main().catch(console.error);
--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import tsconfigPaths from "vite-tsconfig-paths";
 import { defineConfig } from "vitest/config";
 
 export default defineConfig({
+  // @ts-expect-error: Dependency version mismatch
   plugins: [react(), tsconfigPaths()],
   test: {
     globals: true,
