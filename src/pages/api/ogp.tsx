41caaf654ddbef24d3e30f30a4590833d63a9450
Shuntaro Nishizawa <me@shun.technology>
2023-03-06 17:15:41 +0900

    feat: Change OGP image

--- src/pages/api/ogp.tsx
+++ src/pages/api/ogp.tsx
@@ -1,12 +1,12 @@
 import { ImageResponse } from "@vercel/og";
 import type { NextRequest } from "next/server";
 
-const BACKGROUND_IMG = `<svg id="visual" viewBox="0 0 1200 630" width="1200" height="630" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1"><rect width="1200" height="630" fill="#2c363f"></rect><g><g transform="translate(799 10)"><path d="M0 -270.7L234.5 -135.4L234.5 135.4L0 270.7L-234.5 135.4L-234.5 -135.4Z" fill="none" stroke="#58babf" stroke-width="2"></path></g><g transform="translate(140 414)"><path d="M0 -243L210.4 -121.5L210.4 121.5L0 243L-210.4 121.5L-210.4 -121.5Z" fill="none" stroke="#58babf" stroke-width="2"></path></g><g transform="translate(1042 617)"><path d="M0 -248L214.8 -124L214.8 124L0 248L-214.8 124L-214.8 -124Z" stroke="#58babf" fill="none" stroke-width="2"></path></g></g></svg>`;
+import { BACKGROUND_IMG } from "@/assets/background";
 
 const ICON_PATH = new URL("../../assets/icon.jpg", import.meta.url);
 
 const GOOGLE_FONT_URL = "https://fonts.googleapis.com/css2";
-const NOTO_SANS_JP = "Noto Sans JP:wght@700";
+const M_PLUS_1P = "M PLUS Rounded 1c:wght@500";
 
 export const config = {
   runtime: "edge",
@@ -15,14 +15,10 @@ export const config = {
 export default async function ogp(req: NextRequest) {
   const { searchParams } = new URL(req.url);
   const title = searchParams.get("title");
-  const date = searchParams.get("date");
 
   const name = "しゅん🌙";
 
-  const notoSansJp = await fetchFont(
-    NOTO_SANS_JP,
-    `${title ?? ""}${name}${date ?? ""}`,
-  );
+  const mPlus1p = await fetchFont(M_PLUS_1P, `${title ?? ""}${name}`);
 
   const backgroundImage = `url(data:image/svg+xml,${encodeURIComponent(
     BACKGROUND_IMG,
@@ -32,12 +28,12 @@ export default async function ogp(req: NextRequest) {
     (
       <div
         style={{
-          fontSize: 32,
+          fontSize: 36,
           backgroundImage,
           backgroundRepeat: "no-repeat",
           backgroundPosition: "center",
           backgroundSize: "100% 100%",
-          fontFamily: '"NotoSansJP"',
+          fontFamily: '"M PLUS Rounded 1c"',
           color: "#e7f5f6",
           width: "100%",
           height: "100%",
@@ -46,7 +42,7 @@ export default async function ogp(req: NextRequest) {
           flexDirection: "column",
           alignItems: "center",
           justifyContent: "center",
-          padding: "16px 48px 64px",
+          padding: "16px 48px 56px",
         }}
       >
         <h2
@@ -84,8 +80,6 @@ export default async function ogp(req: NextRequest) {
             />
             {name}
           </div>
-
-          {date && <div>{date}</div>}
         </div>
       </div>
     ),
@@ -94,8 +88,8 @@ export default async function ogp(req: NextRequest) {
       height: 630,
       fonts: [
         {
-          name: "NotoSansJP",
-          data: notoSansJp,
+          name: "M PLUS Rounded 1c",
+          data: mPlus1p,
           style: "normal",
         },
       ],
