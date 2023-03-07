709e24191eb3b3a753ce556f2c8b087cab429dd3
Shuntaro Nishizawa <me@shun.technology>
2023-03-07 19:03:52 +0900

    feat: Make math RSC
    resolve #26

--- src/components/markdown/InlineMath.tsx
+++ src/components/markdown/InlineMath.tsx
@@ -1,12 +1,16 @@
-"use client";
-
+import katex from "katex";
 import type { InlineMath as InlineMathNode } from "mdast-util-math";
-import { InlineMath as KatexInlineMath } from "react-katex";
 
 type Props = {
   children: InlineMathNode;
 };
 
 export function InlineMath({ children: { value } }: Props): JSX.Element {
-  return <KatexInlineMath math={value} />;
+  return (
+    <span
+      dangerouslySetInnerHTML={{
+        __html: katex.renderToString(value),
+      }}
+    />
+  );
 }
--- src/components/markdown/Math.tsx
+++ src/components/markdown/Math.tsx
@@ -1,12 +1,16 @@
-"use client";
-
+import katex from "katex";
 import type { Math as MathNode } from "mdast-util-math";
-import { BlockMath as KatexBlockMath } from "react-katex";
 
 type Props = {
   children: MathNode;
 };
 
 export function Math({ children: { value } }: Props): JSX.Element {
-  return <KatexBlockMath math={value} />;
+  return (
+    <div
+      dangerouslySetInnerHTML={{
+        __html: katex.renderToString(value, { displayMode: true }),
+      }}
+    />
+  );
 }
