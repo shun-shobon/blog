41958e28c4c0e613865887f366f887760363ed96
Shuntaro Nishizawa <me@shun.technology>
2023-03-08 04:24:22 +0900

    fix: Tweak OGP

--- src/app/ogp/route.tsx
+++ src/app/ogp/route.tsx
@@ -59,6 +59,7 @@ export async function GET(req: NextRequest): Promise<ImageResponse> {
             textAlign: "center",
             flexGrow: "1",
             lineHeight: 1,
+            rowGap: "8px",
           }}
         >
           {emoji && (
