c5bf473d2c42ce4a32504f46aa9f5b125679b995
Shuntaro Nishizawa <me@shun.technology>
2023-03-13 02:31:53 +0900

    feat: Add hamburger menu

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,10 +1,8 @@
-import { FaGithub, FaHome } from "react-icons/fa";
-
 import { InternalLink } from "@/components/InternalLink";
-import { Link } from "@/components/Link";
 import { TITLE } from "@/config";
 
 import styles from "./Header.module.css";
+import { HeaderMenu } from "./HeaderMenu";
 
 export function Header(): JSX.Element {
   return (
@@ -19,21 +17,7 @@ export function Header(): JSX.Element {
             className={styles.logoImg}
           />
         </InternalLink>
-        <ul className={styles.navList}>
-          <li>
-            <Link href="/" className={styles.navLink}>
-              <FaHome />
-            </Link>
-          </li>
-          <li>
-            <Link
-              href="https://github.com/shun-shobon"
-              className={styles.navLink}
-            >
-              <FaGithub />
-            </Link>
-          </li>
-        </ul>
+        <HeaderMenu />
       </nav>
     </header>
   );
---
+++ src/app/HeaderMenu.tsx
@@ -0,0 +1,57 @@
+"use client";
+
+import classNames from "classnames";
+import { useEffect, useState } from "react";
+import { FaGithub, FaHome } from "react-icons/fa";
+import { RxCross2, RxHamburgerMenu } from "react-icons/rx";
+
+import { Link } from "@/components/Link";
+import { useMediaQuery } from "@/hooks/useMediaQuery";
+
+import styles from "./HeaderMenu.module.css";
+
+export function HeaderMenu(): JSX.Element {
+  const [showMenu, setShowMenu] = useState(false);
+  const matches = useMediaQuery("(max-width: 640px)");
+
+  useEffect(() => {
+    setShowMenu(!matches);
+  }, [matches]);
+
+  return (
+    <>
+      <button
+        onClick={() => setShowMenu((prev) => !prev)}
+        className={classNames(
+          styles.hamburgerButton,
+          showMenu && styles.hamburgerButtonClicked,
+        )}
+      >
+        {showMenu ? <RxCross2 /> : <RxHamburgerMenu />}
+      </button>
+      <ul className={styles.navList} hidden={!showMenu}>
+        {headerMenu.map(({ name, href, Icon }) => (
+          <li key={href}>
+            <Link href={href} className={styles.navLink}>
+              <Icon className={styles.navIcon} />
+              {name}
+            </Link>
+          </li>
+        ))}
+      </ul>
+    </>
+  );
+}
+
+const headerMenu = [
+  {
+    name: "HOME",
+    href: "/",
+    Icon: FaHome,
+  },
+  {
+    name: "GitHub",
+    href: "https://github.com/shun-shobon",
+    Icon: FaGithub,
+  },
+];
