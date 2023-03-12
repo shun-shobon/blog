470b6797adae341c04ffc9b5aa988a24ea54777c
Shuntaro Nishizawa <me@shun.technology>
2023-03-12 22:59:53 +0900

    fix: Fix progressbar when same link is clicked

--- src/components/InternalLink.tsx
+++ src/components/InternalLink.tsx
@@ -1,16 +1,22 @@
 "use client";
 
 import NextLink from "next/link";
+import { usePathname } from "next/navigation";
 import nProgress from "nprogress";
 import type { ComponentProps, MouseEventHandler } from "react";
 
 type Props = ComponentProps<typeof NextLink>;
 
-export function InternalLink({ onClick, ...props }: Props): JSX.Element {
+export function InternalLink({ onClick, href, ...props }: Props): JSX.Element {
+  const pathname = usePathname();
+
   const onClickWrapped: MouseEventHandler<HTMLAnchorElement> = (event) => {
-    nProgress.start();
+    if (href !== pathname) {
+      nProgress.start();
+    }
+
     onClick?.(event);
   };
 
-  return <NextLink onClick={onClickWrapped} {...props} />;
+  return <NextLink onClick={onClickWrapped} href={href} {...props} />;
 }
