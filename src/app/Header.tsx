132e2b4d54a6287e74d11403b3e8e9507417c5f8
Shuntaro Nishizawa <me@shun.technology>
2023-03-13 01:33:39 +0900

    feat(header): Add links

--- src/app/Header.tsx
+++ src/app/Header.tsx
@@ -1,3 +1,5 @@
+import { FaGithub, FaHome } from "react-icons/fa";
+
 import { InternalLink } from "@/components/InternalLink";
 import { TITLE } from "@/config";
 
@@ -6,18 +8,31 @@ import styles from "./Header.module.css";
 export function Header(): JSX.Element {
   return (
     <header className={styles.header}>
-      <nav>
-        <ul className={styles.headerList}>
-          <li className={styles.headerBrand}>
-            <InternalLink href="/" className={styles.headerBrandLink}>
-              <img
-                src="/logo.svg"
-                width={3072}
-                height={512}
-                alt={TITLE}
-                className={styles.headerBrandLogo}
-              />
+      <nav className={styles.nav}>
+        <InternalLink href="/" className={styles.logoLink}>
+          <img
+            src="/logo.svg"
+            width={3072}
+            height={512}
+            alt={TITLE}
+            className={styles.logoImg}
+          />
+        </InternalLink>
+        <ul className={styles.navList}>
+          <li>
+            <InternalLink href="/" className={styles.navLink}>
+              <FaHome />
             </InternalLink>
+          </li>
+          <li>
+            <a
+              href="https://github.com/shun-shobon"
+              target="_blank"
+              rel="norefferer"
+              className={styles.navLink}
+            >
+              <FaGithub />
+            </a>
           </li>
         </ul>
       </nav>
