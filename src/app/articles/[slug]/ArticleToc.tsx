803396ba1a78e202b1508b46a5af25f6738c8ad7
Shuntaro Nishizawa <me@shun.technology>
2023-03-13 18:09:35 +0900

    feat: Add active toc styling

--- src/app/articles/[slug]/ArticleToc.tsx
+++ src/app/articles/[slug]/ArticleToc.tsx
@@ -1,11 +1,13 @@
 "use client";
 
 import classNames from "classnames";
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 
 import { PhrasingContentList } from "@/components/markdown";
 import type { Article } from "@/lib/plugins";
 import type { Toc } from "@/lib/plugins/remark-article/mdast-toc";
+
+import styles from "./ArticleToc.module.css";
 
 type Props = {
   article: Article;
@@ -13,55 +15,86 @@ type Props = {
 };
 
 export function ArticleToc({ article, className }: Props): JSX.Element | null {
+  const [activeIds, setActiveIds] = useState<string[]>([]);
+
   useEffect(() => {
     const observer = new IntersectionObserver(
       (event) => {
-        console.log(event);
+        const id = event.find((e) => e.isIntersecting)?.target.id;
+
+        if (!id) return;
+        setActiveIds([id]);
       },
       {
         root: null,
-        rootMargin: "0px",
-        threshold: [1],
+        rootMargin: "-80px 0px -50% 0px",
+        threshold: [0.0, 1.0],
       },
     );
 
-    article.toc.forEach((item) => {
-      const id = `#${item.heading.identifier ?? "fake"}`;
+    const subscribe = (item: Toc) => {
+      const id = item.heading.identifier ?? "fake";
 
-      const $heading = document.querySelector(id);
+      const $heading = document.querySelector(`#${id}`);
       if (!$heading) return;
 
       observer.observe($heading);
-    });
+      item.children.forEach(subscribe);
+    };
+    article.toc.forEach(subscribe);
+
+    return () => {
+      observer.disconnect();
+    };
   }, [article]);
 
   return (
-    <nav className={classNames(className)}>
-      <ul>
-        {article.toc.map((item) => (
-          <TocItem key={item.heading.identifier ?? ""} toc={item} />
-        ))}
-      </ul>
-    </nav>
+    <div className={className}>
+      <nav className={styles.nav}>
+        <h2 className={styles.title}>目次</h2>
+        <ul>
+          {article.toc.map((item) => (
+            <TocItem
+              key={item.heading.identifier ?? ""}
+              toc={item}
+              activeIds={activeIds}
+            />
+          ))}
+        </ul>
+      </nav>
+    </div>
   );
 }
 
 type TocItemProps = {
   toc: Toc;
+  activeIds: string[];
 };
 
-export function TocItem({ toc }: TocItemProps): JSX.Element {
+export function TocItem({ toc, activeIds }: TocItemProps): JSX.Element {
+  const id = toc.heading.identifier ?? "fake";
+
   return (
-    <li>
-      <a href={`#${toc.heading.identifier ?? ""}`}>
+    <li className={styles.item}>
+      <a
+        href={`#${id}`}
+        className={classNames(
+          styles.link,
+          activeIds.includes(id) && styles.linkActive,
+        )}
+      >
         <PhrasingContentList footnoteDefs={[]}>
           {toc.heading.children}
         </PhrasingContentList>
       </a>
       {toc.children.length > 0 && (
-        <ul>
+        <ul className={styles.list}>
           {toc.children.map((item) => (
-            <TocItem key={item.heading.identifier ?? ""} toc={item} />
+            <TocItem
+              key={item.heading.identifier ?? ""}
+              toc={item}
+              activeIds={activeIds}
+            />
           ))}
         </ul>
       )}
