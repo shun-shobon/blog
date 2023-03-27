a0048e67e9993252e116a909e67463892c91c093
Shuntaro Nishizawa <me@shun.technology>
2023-03-28 03:02:25 +0900

    chore: Remove unnecessary eslint rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,15 +1,12 @@
 /** @type {import("eslint").ESLint.ConfigData} */
 module.exports = {
   root: true,
-  plugins: ["simple-import-sort"],
   parserOptions: {
     project: "./tsconfig.eslint.json",
     tsconfigRootDir: __dirname,
   },
   extends: ["@shun-shobon/base", "next/core-web-vitals", "prettier"],
   rules: {
-    "simple-import-sort/imports": "error",
-    "simple-import-sort/exports": "error",
     "@next/next/no-img-element": "off",
   },
   overrides: [
