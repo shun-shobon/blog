e98eeeadaba97431de204a5fb20bdc3179afb34d
Shuntaro Nishizawa <me@s2n.tech>
2023-08-01 21:46:15 +0900

    feat: Goodbye next-sitemap

--- next-sitemap.config.js
+++
@@ -1,9 +0,0 @@
-/** @type {import("next-sitemap").IConfig} */
-module.exports = {
-  siteUrl: "https://blog.s2n.tech",
-  generateRobotsTxt: true,
-  exclude: ["/server-sitemap.xml", "/ogp", "/feed"],
-  robotsTxtOptions: {
-    additionalSitemaps: ["https://blog.s2n.tech/server-sitemap.xml"],
-  },
-};
